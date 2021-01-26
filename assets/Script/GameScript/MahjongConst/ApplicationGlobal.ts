1f1735dc8ae31616cbefd767c6592d1470cd7332
weishere <wei.huang@hotmail.com>
2021-01-26 19:47:53 +0800

    修改了baseUrl

--- assets/Script/GameScript/MahjongConst/ApplicationGlobal.ts
+++ assets/Script/GameScript/MahjongConst/ApplicationGlobal.ts
@@ -1,4 +1,5 @@
 export class ApplicationGlobal {
     public static GatePanel: cc.Node = null;
-    public static BaseUrl:string = 'http://192.168.101.5:8002'
+    //public static BaseUrl:string = 'http://192.168.101.5:8002'
+    public static BaseUrl:string = 'http://139.9.242.13:8002'
 }
