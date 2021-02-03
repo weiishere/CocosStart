f81ec77e1106ba029e17780a4e35a510757c7df1
weishere <wei.huang@hotmail.com>
2021-02-03 16:36:21 +0800

    加入跑马灯

--- assets/Script/GameScript/Mediator/GatePanelMediator.ts
+++ assets/Script/GameScript/Mediator/GatePanelMediator.ts
@@ -406,7 +406,8 @@ export class GatePanelMediator extends BaseMediator {
                 this.scrollMsgNode = cc.instantiate(cc.loader.getRes(PrefabDefine.ScrollMsgNode, cc.Prefab)) as cc.Node;
                 this.gameStartPanel.addChild(this.scrollMsgNode);
                 this.scrollMsgNode.setPosition(cc.v2(30, 233));
-                this.scrollMsgNode.getComponent('ScrollMsgNode').createContent('抵制不良游戏，拒绝盗版游戏，注意自我保护，谨防受骗上当，适度游戏益脑，沉迷游戏伤身，合理安排时间，享受健康生活', 300);
+                this.scrollMsgNode.getComponent('ScrollMsgNode').createContent('系统公告：' + this.getConfigProxy().leessang, 300);
+                //this.scrollMsgNode.getComponent('ScrollMsgNode').createContent('抵制不良游戏，拒绝盗版游戏，注意自我保护，谨防受骗上当，适度游戏益脑，沉迷游戏伤身，合理安排时间，享受健康生活', 300);
                 break;
             case CommandDefine.OpenLoadingPanel:
                 this.loadingPanel = new cc.Node('Loading');
