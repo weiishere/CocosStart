767bfe6a11d931faa2d61dfc352666cdf21a185f
data@data.com <jackygmr@qq.com>
2021-04-20 20:17:09 +0800

    commit

--- assets/Script/GameScript/Other/SpriteLoadUtil.ts
+++ assets/Script/GameScript/Other/SpriteLoadUtil.ts
@@ -4,12 +4,18 @@ export class SpriteLoadUtil {
      * @param sprite 
      * @param url 
      */
-    static loadSprite(sprite: cc.Sprite, url) {
+    static loadSprite(sprite: cc.Sprite, url: string) {
         let texture = cc.loader.getRes(url, cc.Texture2D);
         if (texture) {
             sprite.spriteFrame = new cc.SpriteFrame(texture);
         } else {
-            cc.loader.load(url, (error, item) => {
+            let extname: string = cc.path.extname(url);
+            if (!extname) {
+                extname = 'png';
+            } else {
+                extname = extname.replace(".", "");
+            }
+            cc.loader.load({ url: url, type: extname }, (error, item) => {
                 if (error) {
                     cc.log("load sprite url : ", error);
                     return;
