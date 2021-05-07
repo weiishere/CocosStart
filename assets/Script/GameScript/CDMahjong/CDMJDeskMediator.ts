66e4f05eff62acbe805b235f214372e34c9eb8c2
data@data.com <jackygmr@qq.com>
2021-05-07 21:29:08 +0800

    commit

--- assets/Script/GameScript/CDMahjong/CDMJDeskMediator.ts
+++ assets/Script/GameScript/CDMahjong/CDMJDeskMediator.ts
@@ -20,11 +20,8 @@ import ChatBox, { MsgObj } from "../Component/DdYiMahjong/ChatBox";
 import { CommandDefine } from "../MahjongConst/CommandDefine";
 import { CDMJMusicManager } from "./CDMJMusicManager";
 import getLocation from "../Util/GetLocation";
-<<<<<<< HEAD
-=======
 import CDMJPosition, { PlayerData } from "./Component/CDMJPosition";
 import { LocalCacheDataProxy } from "../Proxy/LocalCacheDataProxy";
->>>>>>> 4f9a42a0cd83591f8f5232d695d4692c3463efc9
 
 
 
@@ -108,7 +105,8 @@ export default class CDMJDeskMediator extends BaseMediator {
             CDMJCommandDefine.ClearDeskGameView,
             CDMJCommandDefine.OpenCardRecord,
             CDMJCommandDefine.CheckHuCard,
-            CDMJCommandDefine.HuCardListPush
+            CDMJCommandDefine.HuCardListPush,
+            CDMJCommandDefine.ShowLocationPanel
         ];
     }
 
@@ -206,6 +204,8 @@ export default class CDMJDeskMediator extends BaseMediator {
                             this.sendNotification(CDMJCommandDefine.OpenCardRecord);
                         } else if (node.name === 'winCard') {
                             this.sendNotification(CDMJCommandDefine.CheckHuCard);
+                        } else if (node.name === 'location') {
+                            this.sendNotification(CDMJCommandDefine.ShowLocationPanel);
                         }
                     });
                     /**出牌事件 */
@@ -273,7 +273,8 @@ export default class CDMJDeskMediator extends BaseMediator {
 
                 break;
             case CDMJCommandDefine.RefreshPlayerPush:
-                this.DeskPanelViewScript && this.DeskPanelViewScript.updatePlayerHeadView();
+                this.DeskPanelViewScript && this.DeskPanelViewScript.updatePlayerHeadView(true);
+
                 break;
             case CDMJCommandDefine.ExitDeskPanel:
                 this.getDeskProxy().getDeskData().gameSetting.roomId = 0;
@@ -405,7 +406,7 @@ export default class CDMJDeskMediator extends BaseMediator {
                 }
                 break;
             case CDMJCommandDefine.ChangePlayerGold://金币变化
-                this.facade.sendNotification(CDMJCommandDefine.RefreshPlayerPush, {}, '');
+                //this.facade.sendNotification(CDMJCommandDefine.RefreshPlayerPush, {}, '');
                 this.DeskPanelViewScript.showPlayerGlodChange();
                 break;
             case CDMJCommandDefine.DingzhangDone://玩家定章完成
@@ -452,8 +453,6 @@ export default class CDMJDeskMediator extends BaseMediator {
                 const rtMayHuCardBox = <cc.Node>cc.instantiate(cc.loader.getRes(PrefabDefine.RtMayHuCardBox, cc.Prefab));
                 cc.find('Canvas/cdmjdeskView').addChild(rtMayHuCardBox);
                 break;
-<<<<<<< HEAD
-=======
             case CDMJCommandDefine.ShowLocationPanel:
                 const locationPanel = <cc.Node>cc.instantiate(cc.loader.getRes(PrefabDefine.LocationPanel, cc.Prefab));
                 cc.find('Canvas/cdmjdeskView').addChild(locationPanel);
@@ -470,7 +469,6 @@ export default class CDMJDeskMediator extends BaseMediator {
                     this.getDeskProxy().repository.deskData.gameSetting.seatNumber,
                     localCacheDataProxy.getLoginData().userName)
                 break;
->>>>>>> 4f9a42a0cd83591f8f5232d695d4692c3463efc9
         }
     }
 }
