ba2ceb0e7b7be3ba9fce88c62b7f47f7bb423142
data@data.com <jackygmr@qq.com>
2021-05-07 20:37:35 +0800

    commit

--- assets/Script/GameScript/Util/StringUtil.ts
+++ assets/Script/GameScript/Util/StringUtil.ts
@@ -1,5 +1,8 @@
 export class StringUtil {
     static hidePhoneNo(phoneNo: string): string {
+        if (!phoneNo) {
+            return "";
+        }
         return `${phoneNo.substring(0, 3)}****${phoneNo.substring(phoneNo.length - 4, phoneNo.length)}`
     }
 }
